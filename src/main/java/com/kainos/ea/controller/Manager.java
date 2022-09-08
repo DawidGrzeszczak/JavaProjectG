351f6c7d423edf5b37b72574e0b4617d0542804e
kewin.taoanh <kewin.taoanh@kainos.com>
2022-09-08 13:21:10 +0200

    add comparing method

--- src/main/java/com/kainos/ea/controller/Manager.java
+++ src/main/java/com/kainos/ea/controller/Manager.java
@@ -1,4 +1,10 @@
 package com.kainos.ea.controller;
 
+import com.kainos.ea.model.Employee;
+import java.util.List;
+
 public class Manager {
+    public int getHighestSalesTotal(){
+        return 0;
+    };
 }
--- src/main/java/com/kainos/ea/model/SalesEmployee.java
+++ src/main/java/com/kainos/ea/model/SalesEmployee.java
@@ -1,6 +1,19 @@
 package com.kainos.ea.model;
 
-public class SalesEmployee extends Employee{
+public class SalesEmployee extends Employee implements Comparable<SalesEmployee>{
     private double CommissionRate;
     private int TotalSales;
+
+    @Override
+    public int compareTo(SalesEmployee o) {
+        return Float.compare(this.getTotalSales(), o.getTotalSales());
+    }
+
+    public int getTotalSales() {
+        return TotalSales;
+    }
+
+    public void setTotalSales(int totalSales) {
+        TotalSales = totalSales;
+    }
 }
